0b3303ce2c92c5ae0f800f78441ffa587a4b3d32
revronja <37750403+revronja@users.noreply.github.com>
2019-07-04 16:58:01 -0500

    Update GopherRecon.go

--- GopherRecon.go
+++ GopherRecon.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 )
 
-func run_cmd(program string, args ...string) {
+func runCmd(program string, args ...string) {
 	cmd := exec.Command(program, args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -16,8 +17,8 @@ func run_cmd(program string, args ...string) {
 		fmt.Printf("%v\n", err)
 	}
 }
-func checkExeExists(program string) {
-	path, err := exec.LookPath(program)
+func checkExeExists() {
+	path, err := exec.LookPath("ls")
 	if err != nil {
 		fmt.Printf("didn't find 'ls' executable\n")
 	} else {
@@ -25,10 +26,29 @@ func checkExeExists(program string) {
 	}
 }
 
+func runCmdChannels(program string, ch chan<- error, args ...string) {
+	cmd := exec.Command(program, args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		ch <- errors.New(fmt.Sprintf("err %v ", err))
+		fmt.Printf("%v\n", err)
+	}
+
+}
 
 func main() {
 	for _, arg := range os.Args[1:] {
-		run_cmd(os.Args[1], arg)
+		runCmd(os.Args[1], arg)
+	}
+
+	pingErrorChan := make(chan error)
+	if len(os.Args[1:]) >= 3 {
+		for _, arg := range os.Args[1:] {
+			go runCmdChannels(os.Args[1], pingErrorChan, arg)
+		}
 	}
 
 }
